3d610a4c8ed790d63deabf3f543c7dfb421ffd6a
Ramesh Sura <suraramesh46@gmail.com>
2025-07-08 19:00:53 +0530

    Use env for Firebase config

--- server/config/firebaseAdmin.js
+++ server/config/firebaseAdmin.js
@@ -1,20 +1,34 @@
 
+// const admin = require("firebase-admin");
+// const fs    = require("fs");
+
+// // Path to your service account JSON, set in .env as FB_ADMIN_CRED
+// const keyPath = process.env.FB_ADMIN_CRED;
+// if (!keyPath) throw new Error("Missing FB_ADMIN_CRED environment variable");
+
+// // Load and parse service account credentials
+// const serviceJson = JSON.parse(
+//   fs.readFileSync(keyPath, { encoding: "utf8" })
+// );
+
+// // Initialize the default app only once
+// if (!admin.apps.length) {
+//   admin.initializeApp({
+//     credential: admin.credential.cert(serviceJson),
+//   });
+// }
+
+// module.exports = admin;
 const admin = require("firebase-admin");
-const fs    = require("fs");
 
-// Path to your service account JSON, set in .env as FB_ADMIN_CRED
-const keyPath = process.env.FB_ADMIN_CRED;
-if (!keyPath) throw new Error("Missing FB_ADMIN_CRED environment variable");
+const jsonData = process.env.FIREBASE_CONFIG_JSON;
+if (!jsonData) throw new Error("Missing FIREBASE_CONFIG_JSON env variable");
 
-// Load and parse service account credentials
-const serviceJson = JSON.parse(
-  fs.readFileSync(keyPath, { encoding: "utf8" })
-);
+const serviceAccount = JSON.parse(jsonData);
 
-// Initialize the default app only once
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceJson),
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
